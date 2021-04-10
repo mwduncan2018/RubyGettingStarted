9d7f62e854ef72b7aec3144d9ce668ffe233dccd
Michael Duncan <mwduncan2018@gmail.com>
2021-04-10 15:44:59 -0400

    updates

--- test/TestFoobar.rb
+++ test/TestFoobar.rb
@@ -4,6 +4,15 @@ require 'test/unit'
 #Must inherit from TestCase
 class TestFoobar < Test::Unit::TestCase
 
+    #BeforeEach
+    def setup
+        puts "BeforeEach is known as 'setup' in Ruby Test:Unit"
+    end
+    #AfterEach
+    def teardown
+        puts "AfterEach is known as 'teardown' in Ruby Test:Unit"
+    end
+
     #Tests must begin with 'test' or 'test_'
     def test_givenAPersonUnderAge50_whenTheIsOldMethodIsCalled_thenTheMethodShouldReturnFalse
         #Arrange
@@ -33,6 +42,17 @@ class TestFoobar < Test::Unit::TestCase
         assert_equal(29, d.age)
         assert_equal(75000, d.salary)
         assert_equal("Store Manager", d.profession)
+    end
+
+    def test_givenAPersonWithAge20_thenTheAgeIsSavedPlusOrMinusFive
+        p = Foobar::Person.new("Jack","E",20,160000,"Mechanical Engineer")
+        assert_in_delta(15, p.age, 5, "This is a demo of assert_in_delta()")
+        assert_in_delta(25, p.age, 5, "This is a demo of assert_in_delta()")        
+    end
+
+    def test_demonstrateAssertKindOf
+        p = Foobar::Person.new("Jack","E",20,160000,"Mechanical Engineer")
+        assert_kind_of(Foobar::Person, p)
     end
 
 end
